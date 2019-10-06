d99d3c5996039d367c123e237d59d0642737611f
Leroy Banks <54648763+leroybanks@users.noreply.github.com>
2019-10-06 17:12:49 -0500

    Lab

--- Lab7/Invoice1.js
+++ Lab7/Invoice1.js
@@ -23,11 +23,11 @@ var item5 = 'Colegate Classic Toothbrush';
      var price5 = 10;
 
 // Compute extended prices
-var $extended_price1 = $price1 * quantity1;
-var $extended_price2 = $price2 * quantity2;
-var $extended_price3 = $price3 * quantity3;
-var $extended_price4 = $price4 * quantity4;
-var $extended_price5 = $price5 * quantity5;
+var extended_price1 = price1 * quantity1;
+var extended_price2 = price2 * quantity2;
+var extended_price3 = price3 * quantity3;
+var extended_price4 = price4 * quantity4;
+var extended_price5 = price5 * quantity5;
 
 // Compute sub-total
 var $subtotal = $extended_price1 + $extended_price2 + $extended_price3 + $extended_price4 + $extended_price5;
@@ -35,6 +35,7 @@ var $subtotal = $extended_price1 + $extended_price2 + $extended_price3 + $extend
 var taxrate = 0.0575;
 var taxpercent = "5.75" + "%";
 var salestax = (subtotal * taxrate).toFixed(2);
+
 
 var grandtotal = (Number(subtotal) + Number(salestax)).toFixed(2)
 
@@ -58,21 +59,21 @@ document.write("<br>" + "<br>")
 
 document.write(
 "<div>" + 
-    "<table border ="2\">" +
+    "<table border ='2'>"
         "<tbody>" +
             "<tr>" +
-                "<th style=\"text-align: center;\" width=\"43%\">" + "Item" + "</th>" +
-                "<th style=\"text-align: center;\" width=\"11%\">" + "Quantity" + "</th>" +
-                "<th style=\"text-align: center;\" width=\"13%\">" + "Price" + "</th>" +
-                "<th style=\"text-align: center;\" width=\"54%\">" + "Extended Price" + "</th>" +
+                "<th style='text-align: center' width='43%'>" + "Item" + "</th>" +
+                "<th style='text-align: center' width='11%'>" + "Quantity" + "</th>" +
+                "<th style='text-align: center' width='13%'>" + "Price" + "</th>" +
+                "<th style='text-align: center' width='54%'>" + "Extended Price" + "</th>" +
             "</tr>" +
             "</th>" +
             "</tr>" +
             "<tr>"  +
-                "<td width=\"43%\">" " + item1 + "</td>" +
-                "<td align=\"center\" width=\"11%\">" + quantity1 + "</td>" +
-                "<td width=\"13%\">" + price1 + "</td>" +
-                "<td width=\"54%\">" + extended_price1 + "</td>" +
+                "<td width='43%'>" " + item1 + "</td>" +
+                "<td align='center' width='43%'>" + quantity1 + "</td>" +
+                "<td width='43%'>" + price1 + "</td>" +
+                "<td width='43%'>" + extended_price1 + "</td>" +
             "</tr>" +
             "<tr>" +
                 "<td width=\"43%\">" + item2 + "</td>" +
