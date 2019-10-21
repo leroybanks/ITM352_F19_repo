86a81cb7445b280cf8cb237c26dc919946b8c37f
Leroy Banks <54648763+leroybanks@users.noreply.github.com>
2019-10-20 20:59:12 -1000

    Lab11

---
+++ Lab11/isNonNegInt.js
@@ -0,0 +1,25 @@
+/*
+Purpose: function to determine if a string is a non-negative interger
+Date: 10/18/2019
+Author: Leroy Banks
+
+Revions history:
+Usage notes:
+*/
+
+function isNonNegInt(q, returnErrors = false)
+{
+    errors = []; // assume no errors at first
+    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+
+    if (sendArrayBack)
+    {
+        return (errors);
+    } else
+    {
+    return(errors.length == 0);
+    }
+console.log(isNonNegInt("-3"));
+}
---
+++ Lab11/pieces.js
@@ -0,0 +1,32 @@
+/*
+Purpose: function to determine if a string is a non-negative interger
+Date: 10/18/2019
+Author: Leroy Banks
+
+Revions history:
+Usage notes:
+*/
+
+function isNonNegInt(q)
+{
+    errors = []; // assume no errors at first
+    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+
+    return(errors.length == 0);
+}
+
+console.log(isNonNegInt("9"));
+
+attributes = "Leroy; 22;22.5>;-22.5";
+separator = ";";
+pieces = attribues.split(separator);
+
+for  (i=0; i<pieces.length; i++)
+{
+     console.log(`${typeof(pieces[i])} $(pieces[i]} ${isNotNegInt(pieces[i])}`);
+}
+
+recon = pieces.join(separator);
+console.log(recon);
