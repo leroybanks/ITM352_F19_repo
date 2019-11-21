8a7b7c9523376745d0b46e0138b1868425253c89
Leroy Banks <54648763+leroybanks@users.noreply.github.com>
2019-11-21 11:26:00 -1000

    hey

--- Lab14/Ex1.js
+++ Lab14/Ex1.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 var myparser = require('body-parser');
 
 var filename ="user_data.json"
+
+//Only open the file if it exists
+if (fs.existsSync(filemane))
+
 var raw_data = fs.readFileSync(filename, 'utf-8');
-var data = JSON.parse(raw_data);
-console.log(data.dort.password);
+var users_reg_data = JSON.parse(raw_data);
+console.log(users_reg_data);
